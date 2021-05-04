3d1e3db702074f14605d8db4da30e9e6c0a8ee8f
Ilbien Paul <paulilb@students.zhaw.ch>
2021-05-04 06:17:48 +0200

    App

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import CapturePage from './pages/CapturePage/CapturePage';
+import Navigation from './components/Navigation/Navigation';
+import TrainingPage from './pages/TrainningPage/TrainingPage';
+import ExamPage from './pages/ExamPage/ExamPage';
+
+interface AppState {
+  isExamInProgress: boolean;
+}
+
+class App extends React.PureComponent<{},AppState> {
+
+  constructor(props: {}){
+    super(props)
+
+    this.state={
+      isExamInProgress: false,
+    }
+  }
+
+  private isExamInProgress(isExam: boolean) {
+    this.setState({isExamInProgress: isExam});
+  }
+
+  render() {
+    return (
+      <Router>
+    <Navigation isExam={this.state.isExamInProgress}/>
+      <Switch>
+            <Route path="/exam" exact component={() => <ExamPage isExam={this.isExamInProgress.bind(this)}/>}/>
+            <Route path="/training" exact component={TrainingPage}/>
+            <Route path="/exam" exact component={() => <ExamPage isExam={this.isExamInProgress.bind(this)}/>}/>
+            <Route path="/" exact component={CapturePage}/>
+      </Switch>
+    </Router>
+    )
+  }
+}
+
+export default App;
